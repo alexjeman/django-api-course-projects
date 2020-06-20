6514dea71d7d8ffbf806d0ddecea3af85703b902
alexjeman <alexandrujeman.ja@gmail.com>
2020-06-20 10:38:28 +0300

    Add django headers allowed

--- react-django-project/server/settings/settings.py
+++ react-django-project/server/settings/settings.py
@@ -134,3 +134,18 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'token',
+    'cache-control'
+)
