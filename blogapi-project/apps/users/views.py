4355d6d1fe3c7e53adbe19b0883663d4a9beef7c
alexjeman <alexandrujeman.ja@gmail.com>
2020-06-20 21:29:36 +0300

    Extend users

---
+++ blogapi-project/apps/users/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+
+from apps.users.serializers import UserSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
